5444d77e211cdbc3cf2ce68ee0fdd0d86d3e2d99
roshaemarion <65013844+roshaemarion@users.noreply.github.com>
2020-05-30 17:14:01 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,23 @@
+class Chain {
+    constructor (bodyA,bodyB){
+        var options = {
+            bodyA : bodyA, 
+             bodyB : bodyB,
+             stiffness : 0.04,
+             length : 10
+        }
+        this.chain = Matter.Constraint.create(options)
+        World.add(world, this.chain)
+        
+    }
+    display(){
+    strokeWeight(5) 
+    stroke("blue")   
+    line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+    
+
+    }
+
+
+
+}
